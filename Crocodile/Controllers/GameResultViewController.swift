85b0f11fbd4ce81e6e368fea9849acb083d35474
kovs705 <smartkovsik@gmail.com>
2023-04-17 17:54:41 +0300

    initial commit

---
+++ Crocodile/Controllers/GameResultViewController.swift
@@ -0,0 +1,8 @@
+//
+//  GameResultViewController.swift
+//  Crocodile
+//
+//  Created by Kovs on 17.04.2023.
+//
+
+import Foundation
---
+++ Crocodile/Controllers/WrongViewController.swift
@@ -0,0 +1,8 @@
+//
+//  WrongViewController.swift
+//  Crocodile
+//
+//  Created by Kovs on 17.04.2023.
+//
+
+import Foundation
