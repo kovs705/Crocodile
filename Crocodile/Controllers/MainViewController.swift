a562a59683b9ac4e814dfff15c82ef0baf0ee30d
Amsterdance26 <amsterdance26@gmail.com>
2023-04-19 15:43:30 +0700

    added background

--- Crocodile/Controllers/MainViewController.swift
+++ Crocodile/Controllers/MainViewController.swift
@@ -6,16 +6,23 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // test
-        
+
+        view.addSubview(backgroundImage)
+        backgroundImage.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
 
-
+    let backgroundImage: UIImageView = {
+        let image = UIImageView(image: UIImage(named: "backgroundImage"))
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
 }
 
