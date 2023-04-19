b3c13e56d0d709a7071f7cd195bfad3c6bd76cb3
BelyahRU <sanya007.andreev@yandex.ru>
2023-04-20 00:38:40 +0300

    make background

--- Crocodile/Controllers/CorrectViewController.swift
+++ Crocodile/Controllers/CorrectViewController.swift
@@ -1,8 +1,22 @@
-//
-//  CorrectViewController.swift
-//  Crocodile
-//
-//  Created by Kovs on 17.04.2023.
-//
-
 import Foundation
+import UIKit
+class CorrectViewController: UIViewController {
+    
+    let backgroundImage = UIImageView(image: UIImage(named: Resources.Image.backgroundImage))
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupBackgroundImage()
+    }
+    func setupBackgroundImage() {
+        view.addSubview(backgroundImage)
+        backgroundImage.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+        }
+    }
+    
+}
