5a243c473647a4ca54e5b979b412c0336354a487
Amsterdance26 <amsterdance26@gmail.com>
2023-04-19 17:58:38 +0700

    added rules

--- Crocodile/Controllers/MainViewController.swift
+++ Crocodile/Controllers/MainViewController.swift
@@ -12,12 +12,17 @@ class MainViewController: UIViewController {
 
     let backgroundImage = UIImageView(image: UIImage(named: Resources.Image.backgroundImage))
     let crocodileImage = UIImageView(image: UIImage(named: Resources.Image.crocodileImage))
+    let startGameButton = GreenButton(title: "Старт игры")
+    let rulesButton = GreenButton(title: "Правила игры")
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupBackgroundImage()
         setupCrocodileImage()
+        setupStartGameButton()
+        setupRulesGameButton()
+
         
     }
 
@@ -37,6 +42,26 @@ class MainViewController: UIViewController {
             make.top.equalToSuperview().offset(172)
             make.left.equalToSuperview().offset(54)
             make.right.equalToSuperview().offset(-54)
+        }
+    }
+
+    func setupStartGameButton() {
+        view.addSubview(startGameButton)
+        startGameButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(crocodileImage.snp.bottom).offset(48)
+            make.width.equalTo(274)
+            make.height.equalTo(83)
+        }
+    }
+
+    func setupRulesGameButton() {
+        view.addSubview(rulesButton)
+        rulesButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(startGameButton.snp.bottom).offset(48)
+            make.width.equalTo(211)
+            make.height.equalTo(63)
         }
     }
 }
